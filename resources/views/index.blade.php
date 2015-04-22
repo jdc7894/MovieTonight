13c2c9e39372ad9a2edcfcf3776f03d8eaa8760a
Decheol Jeong <bgedechul@gmail.com>
2015-04-21 22:19:43 -0700

    Initial commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,11 @@
 @extends('layout')
 @section('content')
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+    <div class="container">
+        <div class="row">
+            <h2>This template is using Bootstrap from a CDN!</h2>
+        </div>
+    </div>
     <p> Hi there! </p>
+
 @stop
